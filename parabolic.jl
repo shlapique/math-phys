26ee0c7b734606b37656467199796dd289b66d12
shlapique <gordsdima@gmail.com>
2023-11-24 12:21:03 +0300

    explicit almost done

--- parabolic.jl
+++ parabolic.jl
@@ -21,16 +21,6 @@ function sol(x, t)
     return exp(-0.5 * t) * sin.(x)
 end
 
-function get_sol(x, t)
-    solution = zeros(0)
-    for i in x
-        for j in t
-            append!(solution, sol(i, j))
-        end
-    end
-    return solution
-end
-
 function run_through(A, b)
     n = size(A)[1]
     print(n)
@@ -49,7 +39,6 @@ function run_through(A, b)
         Q[i] = (b[i] - A[i, i - 1] * Q[i - 1]) / (A[i, i] + A[i, i - 1] * P[i - 1])
     end
     x[n] = Q[n]
-    println("KROL")
 
     for i = n-1:-1:1
         x[i] = P[i] * x[i + 1] + Q[i]
@@ -57,9 +46,34 @@ function run_through(A, b)
     return x
 end
 
-function explicit(K, t, τ, h, a, c, x, eps)
+function explicit(K, t, τ, σ, h, x, eps)
+    # N = length(x)
+    U = zeros(K, N)
 
+    for j in 1:N
+        # inital cond
+        U[1, j] = psi(x[j])
+    end
+    for k in 2:length(t)-1
+        U[k, 1] = phi0(t[k])
+        for j in 2:length(x)-1
+            U[k, j] = σ * U[k-1, j-1] + (1 - 2*σ) * U[k-1, j] + σ * U[k-1, j+1]
+        end
+        U[k, end] = phi1(t[k])
+        # if eps == 1
+        #     U[k + 1, 0] = U[k + 1, 1] - h * phi0(t)
+        #     U[k + 1, N - 1] = phi0(t)
+        # elseif eps == 2
+        #     U[k + 1, 0] = (2 * h * phi0(t) - 4 * U[k + 1, 1] + U[k + 1, 2]) / (2 * h - 3)
+        #     U[k + 1, N - 1] = phi0(t)
+        # elseif eps == 3
+        #     U[k + 1, 0] = (phi0(t) * h * τ * 2 - U[k + 1, 1] * (2 * τ) - U[k, 0] * h^2) / (-2 * τ - h^2 + c * τ * h^2 + h * τ * 2)
+        #     U[k + 1, N - 1] = phi0(t)
+        # end
+    end
+    return U
 end
+            
 
 l = pi
 a = 1
@@ -83,11 +97,19 @@ t = range(0, T, step=τ)
 # println("Enter t:")
 # t_cur = readline()
 # t_cur = parse(Float64, t_cur)
-t_cur = 1 
+t_cur = 0 
 U = sol(x, t_cur)
 display(U)
 
-plot(x, U)
+if a * τ / h^2 <= 0.5
+    println("Условие Куррента выполнено:", a * τ / h^2, "<= 0.5\n")
+    U2 = explicit(K, t, τ, σ, h, x, eps)
+    plot(x, U2[3000, :])
+end
+
+plot!(x, U)
+
+
 
 # mesh = collect(Iterators.product(x, t))
 # display(mesh)
